3d204b301f0b1aa26cff7eeb707ce59e114bb36a
Gerjan Wielink <gerjan.wielink@nedap.com>
2020-12-02 10:19:55 +0100

    cleanup

--- src/main/kotlin/exercises/day2.kt
+++ src/main/kotlin/exercises/day2.kt
@@ -11,20 +11,17 @@ fun main() {
 
 fun isValidPasswordFirst(line: String) : Boolean {
   val (minStr, maxStr, char, password) = unpackPasswordLine(line)
-  val occurrences = password.count { it.toString() == char}
-  return  minStr.toInt() <= occurrences && occurrences <= maxStr.toInt()
+
+  return  password.count { it.toString() == char} in (minStr.toInt() .. maxStr.toInt())
 }
 
 fun isValidPasswordSecond(line: String) : Boolean {
   val (pos1, pos2, char, password) = unpackPasswordLine(line)
 
-  return listOf(
-    password[pos1.toInt() - 1].toString() == char,
-    password[pos2.toInt() - 1].toString() == char,
-  ).count{ it } == 1
+  return (password[pos1.toInt() - 1].toString() == char) != (password[pos2.toInt() - 1].toString() == char)
 }
 
-fun unpackPasswordLine(line: String) : List<String> = "^(\\d+)-(\\d+)\\s*([a-z]):\\s([a-z]*)"
+fun unpackPasswordLine(line: String) : List<String> = "^(\\d+)-(\\d+) ([a-z]): ([a-z]+)$"
   .toRegex()
   .find(line)!! // :')
   .destructured
