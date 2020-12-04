248926fb9359f0a33711637d44dcee924b93d63d
Gerjan Wielink <gerjan.wielink@nedap.com>
2020-12-04 09:34:25 +0100

    :eyeglasses:

--- src/main/kotlin/exercises/day4.kt
+++ src/main/kotlin/exercises/day4.kt
@@ -26,14 +26,14 @@ fun simpleValidator(entry: String) = entry.matches(
  * Decided to throw all readability overboard to go full Regex :)
  */
 fun advancedValidator(entry: String) = entry.split(":").let { (key, value) ->
-  when (key) {
-    "byr" -> value.matches(Regex("""(19[2-9]\d)|(200[0-2])$""")) // 1929-2002
-    "iyr" -> value.matches(Regex("""^20((1\d)|20)$""")) // 2010-2020
-    "eyr" -> value.matches(Regex("""^20((2\d)|30)$""")) // 2020-2030
-    "hgt" -> value.matches(Regex("""^(1(([5-8]\d)|(9[0-3]))cm)|(((59)|(6\d)|(7[0-6]))in)$""")) //  150-193cm || 59-76in
-    "hcl" -> value.matches(Regex("""^#[\da-f]{6}$"""))
-    "ecl" -> value.matches(Regex("""^(amb|blu|brn|gry|grn|hzl|oth)$"""))
-    "pid" -> value.matches(Regex("""^\d{9}$"""))
-    else -> false // ignoring cid and always setting it to false allows to just check for length 7
-  }
+  value.matches(when (key) {
+    "byr" -> """(19[2-9]\d)|(200[0-2])$""".toRegex() // 1929-2002
+    "iyr" -> """^20((1\d)|20)$""".toRegex() // 2010-2020
+    "eyr" -> """^20((2\d)|30)$""".toRegex() // 2020-2030
+    "hgt" -> """^(1(([5-8]\d)|(9[0-3]))cm)|(((59)|(6\d)|(7[0-6]))in)$""".toRegex() //  150-193cm || 59-76in
+    "hcl" -> """^#[\da-f]{6}$""".toRegex()
+    "ecl" -> """^(amb|blu|brn|gry|grn|hzl|oth)$""".toRegex()
+    "pid" -> """^\d{9}$""".toRegex()
+    else -> """(?=a)b""".toRegex() // always fails as it looks for a character which is both an 'a' and 'b'
+  })
 }
