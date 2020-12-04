aaf37b95f3657532ed40ccb60d238a7e03630815
Gerjan Wielink <gerjan.wielink@nedap.com>
2020-12-04 09:23:27 +0100

    :eyeglasses:

--- src/main/kotlin/exercises/day4.kt
+++ src/main/kotlin/exercises/day4.kt
@@ -22,12 +22,15 @@ fun simpleValidator(entry: String) = entry.matches(
   """^(byr|iyr|eyr|hgt|hcl|ecl|pid):.*$""".toRegex()
 )
 
+/**
+ * Decided to throw all readability overboard to go full Regex :)
+ */
 fun advancedValidator(entry: String) = entry.split(":").let { (key, value) ->
   when (key) {
     "byr" -> value.matches(Regex("""(19[2-9]\d)|(200[0-2])$""")) // 1929-2002
     "iyr" -> value.matches(Regex("""^20((1\d)|20)$""")) // 2010-2020
     "eyr" -> value.matches(Regex("""^20((2\d)|30)$""")) // 2020-2030
-    "hgt" -> value.matches(Regex("""^((1(([5-8]\d)|(9[0-3]))cm)|((59)|(6\d)|(7[0-6]))in)$""")) // 59in-76in || 150cm - 193cm
+    "hgt" -> value.matches(Regex("""^((1(([5-8]\d)|(9[0-3]))cm)|((59)|(6\d)|(7[0-6]))in)$""")) //  150-193cm || 59-76in
     "hcl" -> value.matches(Regex("""^#[0-9a-f]{6}$"""))
     "ecl" -> value.matches(Regex("""^(amb|blu|brn|gry|grn|hzl|oth)$"""))
     "pid" -> value.matches(Regex("""^\d{9}$"""))
