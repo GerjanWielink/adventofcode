9e5ae7827217a314c026560c235e63adc7375fc1
Gerjan Wielink <gerjan.wielink@nedap.com>
2020-12-05 18:59:49 +0100

    Aaaaah :)

--- src/main/kotlin/exercises/day5.kt
+++ src/main/kotlin/exercises/day5.kt
@@ -1,23 +1,15 @@
 package exercises
 
 import io.loadFileAsStrings
-
+// 832, 517
 fun main () {
   val seatIds = loadFileAsStrings("day5")
-    .map {
-      8 * it.slice(0..6).replace('F', '0').replace('B', '1').toInt(2) +
-      it.slice(7..9).replace('R', '1').replace('L', '0').toInt(2)
-    }
+    .map { it.map { c -> if (c in setOf('B', 'R')) '1' else '0' }.joinToString("").toInt(2) }
 
   // part 1
-  seatIds
-    .maxOrNull()
-    ?.let { println("Part 1: $it") }
+  seatIds.maxOrNull()?.let { println("Part 1: $it") }
 
   // part 2
-  seatIds
-    .sorted().zipWithNext()
-    .find { it.second - it.first != 1 }
-    ?.let { println("Part 2: ${it.first + 1}") }
+  seatIds.sorted().zipWithNext().find { it.second - it.first != 1 }?.let { println("Part 2: ${it.first + 1}") }
 
 }
