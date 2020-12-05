a7d55514cdd80c951630029441d61d7eb2e7aecd
Gerjan Wielink <gerjan.wielink@nedap.com>
2020-12-05 07:55:12 +0100

    :eyeglasses:

--- src/main/kotlin/exercises/day5.kt
+++ src/main/kotlin/exercises/day5.kt
@@ -3,28 +3,19 @@ package exercises
 import io.loadFileAsStrings
 
 fun main () {
-  loadFileAsStrings("day5")
-//    listOf("BFFFBBFRRR", "FFFBBBFRRR", "BBFFBBFRLL")
+  val seatIds = loadFileAsStrings("day5")
     .map {
       8 * it.slice(0..6).replace('F', '0').replace('B', '1').toInt(2) +
       it.slice(7..9).replace('R', '1').replace('L', '0').toInt(2)
     }
-    .let { findMissing(it) }
-    .let(::println)
+
+  seatIds
+    .maxOrNull()
+    ?.let { println("Part 1: $it") }
+
+  seatIds
+    .sorted().zipWithNext()
+    .find { it.second - it.first != 1 }
+    ?.let { println("Part 2: ${it.first + 1}") }
 
 }
-
-fun findMissing (list: List<Int>) : Int? {
-  ((list.minOrNull() ?: 0).. (list.maxOrNull() ?: Int.MAX_VALUE)).forEach {
-    if(!list.contains(it)) {
-      return it
-    }
-  }
-
-  return null
-}
-
-// (1, 1) => 9
-// (1, 2) => 10
-// (1, 8) => 16
-// (2, 1) => 17
