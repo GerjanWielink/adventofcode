82a2bf749083ee501c6b0bf84173770ed8c48ab8
Gerjan Wielink <gerjan.wielink@nedap.com>
2020-12-09 09:46:38 +0100

    :eyeglasses:

--- src/main/kotlin/exercises/day9.kt
+++ src/main/kotlin/exercises/day9.kt
@@ -5,20 +5,19 @@ import io.loadFileAsLongs
 fun main() {
   val input = loadFileAsLongs("day9")
 
-  val target = input.windowed(26).find { find2sum(it.dropLast(1), it.last()) == null }?.last() ?: error("No sum found..")
-  println("Part 1: $target")
+  findBuggedNumber(input)?.let {
+    println("Part 1: $it")
 
-  var windowSize = 2
-  run {
-    while (windowSize < input.size) {
-      input.windowed(windowSize)
-        .find { it.reduce(Long::plus) == target }
-        ?.let {
-          it.sorted().let { sorted -> println("Part 2: ${sorted.first() + sorted.last()}") }
-          return@run
-        }
-      windowSize++
-    }
+    println("Part 2: ${findEncryptionWeakness(input, it)}")
   }
-
 }
+
+fun findBuggedNumber(input: List<Long>, preamble: Int = 25) =
+  input.windowed(preamble + 1).find { find2sum(it.dropLast(1), it.last()) == null }?.last()
+
+fun findEncryptionWeakness(input: List<Long>, buggedNumber: Long, windowSize: Int = 2): Long =
+  input.windowed(windowSize).find { it.reduce(Long::plus) == buggedNumber }
+    ?.let { window ->
+      window.sorted().let { it.first() + it.last() }
+    }
+    ?: findEncryptionWeakness(input, buggedNumber, windowSize + 1)
